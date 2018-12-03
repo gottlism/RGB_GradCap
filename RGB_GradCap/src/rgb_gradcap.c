8f03f069f4a0b7ec0f8430bc472ac6505673f269
coopervgarrison <cooper.garrison@vanderbilt.edu>
2018-12-02 18:29:04 -0600

    final changes

--- RGB_GradCap/src/rgb_gradcap.c
+++ RGB_GradCap/src/rgb_gradcap.c
@@ -5,161 +5,169 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/time.h>
+#include <sys/types.h>
+#include <dirent.h>
 
-#include "libsoc_pwm.h"
 #include "libsoc_gpio.h"
 #include "libsoc_debug.h"
+#include "ImageSend.h"
+#include "downsample.h"
 
-typedef FILE * RenderedImage; //Data of image to be rendered, this type might change
-static int N = 20; //Size of command buffer
-static int NIPIN = 20;
-static int SCPIN = 21;
-static int SOPIN = 22;
+typedef char * RenderedImage; //Data of image to be rendered, this type might change
+static int N = 20; //Max size of command buffer
+static int NIPIN = 67;
+static int SCPIN = 44;
+static int SOPIN = 68;
 static int parallelState = 0;
-static pthread_t readCommands, nextImageT, stateChangeT, shiftOrientationT;
-
-typedef struct RawImages
-{
-	char ** filePaths;
-	int currentFile;
-	int arraySize;
-} RawImages;
-
-typedef struct RenderedImages
-{
-	RenderedImage node1;
-	RenderedImage node2;
-} RenderedImages;
+static int orientation = 1;
+static pthread_t readCommands, nextImageT, stateChangeT, shiftOrientationT, resizeT;
 
 typedef struct CommandBuffer
 {
 	char * command;
 	int read;
-    int write;
-    int count;
-    pthread_mutex_t buffer_mutex;
-    pthread_cond_t buffer_cond;
+  int write;
+  int count;
+  pthread_mutex_t buffer_mutex;
+  pthread_cond_t buffer_cond;
 } CommandBuffer;
 
 typedef struct ThreadData
 {
-	RawImages * rawImages;
-	RenderedImages * renderedImages;
+	RenderedImage * renderedImages;
 	CommandBuffer * buffer;
+	int renderedNumber;
 } ThreadData;
 
+/*Moves to the next image*/
 static void * nextImageButton(void * bufferArg) {
   gpio * pin;
   pin = libsoc_gpio_request(NIPIN, LS_GPIO_SHARED);
   libsoc_gpio_set_direction(pin, INPUT);
-  int t;
-  int level;
-  level = 0;
-  int prev_level;
-  prev_level = 0;
+  int t, level = 0, prev_level = 0;
   while(1) {
       level = libsoc_gpio_get_level(pin);
-      if(level==0 && prev_level == 1) {
-          t++;
-          enqueue((struct CommandBuffer *)bufferArg, "next image");
-          // printf("%s\n", "next image pressed");
+      if(level == 0 && prev_level == 1) {
+          ++t;
+          enqueue((struct CommandBuffer *)bufferArg, 'n');
       }
       prev_level = level;
-      //printf("%d\n", level);
       usleep(100);
   }
   libsoc_gpio_free(pin);
 }
 
+/*Changes the state between single and parallel*/
 static void * stateChangeButton(void * bufferArg) {
 	gpio * pin;
 	pin = libsoc_gpio_request(SCPIN, LS_GPIO_SHARED);
 	libsoc_gpio_set_direction(pin, INPUT);
-	int t;
-	int level;
-	level = 0;
-	int prev_level;
-	prev_level = 0;
+	int t, level = 0, prev_level = 0;
 	while(1) {
 			level = libsoc_gpio_get_level(pin);
 			if(level==0 && prev_level == 1) {
-					t++;
-					enqueue((struct CommandBuffer *)bufferArg, "state change");
-					// printf("%s\n", "state change pressed");
+					++t;
+					enqueue((struct CommandBuffer *)bufferArg, 's');
 			}
 			prev_level = level;
-			//printf("%d\n", level);
 			usleep(100);
 	}
 	libsoc_gpio_free(pin);
 }
 
+/*Changes which screen the parallel gifs are displayed on*/
 static void * shiftOrientationButton(void * bufferArg) {
 	gpio * pin;
   pin = libsoc_gpio_request(SOPIN, LS_GPIO_SHARED);
   libsoc_gpio_set_direction(pin, INPUT);
-  int t;
-  int level;
-  level = 0;
-  int prev_level;
-  prev_level = 0;
+	int t, level = 0, prev_level = 0;
   while(1) {
       level = libsoc_gpio_get_level(pin);
       if(level==0 && prev_level == 1) {
-          t++;
-          enqueue((struct CommandBuffer *)bufferArg, "orientation shift");
-          // printf("%s\n", "shift orientation pressed");
+          ++t;
+          enqueue((struct CommandBuffer *)bufferArg, 'o');
       }
       prev_level = level;
-      //printf("%d\n", level);
       usleep(100);
   }
   libsoc_gpio_free(pin);
 }
 
-//Read button input
+//Read and handle button input
 static void * readCommandsThread(void * dataPack) {
 	struct ThreadData * tempDataPack = (struct ThreadData *)dataPack;
-	char * cmd;
+	char cmd;
+	int curImage = 0;
+	dispImage(tempDataPack->renderedImages, curImage);
 	while (1) {
-		pthread_mutex_lock (&(tempDataPack->buffer->buffer_mutex));
-		if (tempDataPack->buffer->count == 0) {
-			pthread_cond_wait (&(tempDataPack->buffer->buffer_cond), &(tempDataPack->buffer->buffer_mutex));
+		dequeue(tempDataPack->buffer, &cmd);
+		//Handle NextImage command
+		if (cmd == 'n') {
+			if(++curImage > tempDataPack->renderedNumber) {
+			curImage = 0;
 		}
-		dequeue(tempDataPack->buffer, cmd);
-		pthread_cond_broadcast (&(tempDataPack->buffer->buffer_cond));
-		pthread_mutex_unlock (&(tempDataPack->buffer->buffer_mutex));
-
-		if (strcmp(cmd, "next image") == 0) {
-			nextImage(&(tempDataPack->rawImages));
+	  	dispImage(tempDataPack->renderedImages, curImage);
 		}
-
-		else if (strcmp(cmd, "state change") == 0) {
-			changeState(&(tempDataPack->rawImages));
+		//Handle StateChange command
+		else if (cmd == 's') {
+			parallelState = !parallelState;
+			dispImage(tempDataPack->renderedImages, curImage);
 		}
-
-		else if (strcmp(cmd, "orientation shift") == 0) {
-			shiftOrientation(&(tempDataPack->rawImages));
+		//Handle ShiftOrientation command
+		else if (cmd == 'o') {
+			orientation = !orientation;
+			dispImage(tempDataPack->renderedImages, curImage);
 		}
+	sleep(1);
 	}
 }
 
+/*Downsamples all images in the unrenderedImages folder*/
+static void * resize(void * dataPack) {
+	struct ThreadData * tempDataPack = (struct ThreadData *)dataPack;
+	DIR *d;
+	struct dirent *dir;
+	d = opendir("./unrenderedImages");
+  while ((dir = readdir(d)) != NULL)
+  {
+  	if ( !strcmp(dir->d_name, ".") || !strcmp(dir->d_name, "..") ) {
+		} else {
+			char input[100];
+			char outputS[100];
+			char outputL[100];
+			char outputR[100];
+			snprintf(&input, sizeof(input) + 1, "%s%s", "./unrenderedImages/", dir->d_name);
+			char * tempName = dir->d_name;
+			tempName[strlen(tempName)-4] = 0;
+			snprintf(&outputS, sizeof(input) + 1, "%s%s%s", "./renderedImages/", tempName, "_S.gif");
+			snprintf(&outputL, sizeof(input) + 1, "%s%s%s", "./renderedImages/", tempName, "_L.gif");
+			snprintf(&outputR, sizeof(input) + 1, "%s%s%s", "./renderedImages/", tempName, "_R.gif");
+			 downsample(&input, &outputS, &outputL,  &outputR);
+			tempDataPack->renderedImages[++tempDataPack->renderedNumber] = tempName;
+		}
+  }
+  closedir(d);
+}
+
 int main (void) {
-	struct RawImages * imagePaths;
-	loadInImages(&imagePaths);
-	struct CommandBuffer * cmdBuf;
-	struct RenderedImages * rendered;
+		struct CommandBuffer cmdBuf;
+	commandBuffer_init(&cmdBuf);
+	RenderedImage rendered[100];
+	struct ThreadData dataPack;
+	dataPack.renderedImages = rendered;
+	dataPack.buffer = &cmdBuf;
+	dataPack.renderedNumber = -1;
 
-	struct ThreadData * dataPack;
-	dataPack->renderedImages = rendered;
-	dataPack->rawImages = imagePaths;
-	dataPack->buffer = cmdBuf;
-
-	pthread_create(&nextImageT, (pthread_attr_t*)0, nextImageButton, &(dataPack->buffer));
-	pthread_create(&stateChangeT, (pthread_attr_t*)0, stateChangeButton, &(dataPack->buffer));
-	pthread_create(&shiftOrientationT, (pthread_attr_t*)0, shiftOrientationButton, &(dataPack->buffer));
+	pthread_create(&resizeT, (pthread_attr_t*)0, resize, &dataPack);
+	while(dataPack.renderedNumber == -1) {
+		sleep(2);
+	}
+	pthread_create(&nextImageT, (pthread_attr_t*)0, nextImageButton, dataPack.buffer);
+	pthread_create(&stateChangeT, (pthread_attr_t*)0, stateChangeButton, dataPack.buffer);
+	pthread_create(&shiftOrientationT, (pthread_attr_t*)0, shiftOrientationButton, dataPack.buffer);
 	pthread_create(&readCommands, (pthread_attr_t*)0, readCommandsThread, &dataPack);
 
+	pthread_join(resizeT, 0);
 	pthread_join(nextImageT, 0);
 	pthread_join(stateChangeT, 0);
 	pthread_join(shiftOrientationT, 0);
@@ -167,75 +175,48 @@ int main (void) {
 
 }
 
-//Looks into directory and loads image file paths into Raw Images
-void loadInImages(RawImages *r) {
-	//doAction
-	r-> arraySize = 0; //Change this to number of files in directory
-	r->currentFile = 0;
-
-}
-
+/*Initializes the command buffer*/
 void commandBuffer_init(CommandBuffer *b) {
-	b->command = (malloc (N*sizeof (char)));
+	b->command = malloc(N*sizeof(char));
 	b-> read = 0;
 	b->write = 0;
 	b->count = 0;
-	pthread_mutex_init(&(b->buffer_mutex),NULL);
-	pthread_cond_init (&(b->buffer_cond), NULL);
+	pthread_mutex_init(&b->buffer_mutex,NULL);
+	pthread_cond_init(&b->buffer_cond, NULL);
 
 }
 
+/*Adds char command into the buffer b*/
 void enqueue (CommandBuffer *b, char command) {
-    pthread_mutex_lock (&(b->buffer_mutex));
-    while (b->count >= N) {
-        pthread_cond_wait (&(b->buffer_cond), &(b->buffer_mutex));
-    }
-    b->command[b->write] = command;
-    b->count++;
-    b->write = (b->write + 1) % N;
-    pthread_cond_broadcast (&(b->buffer_cond));
-    pthread_mutex_unlock (&(b->buffer_mutex));
+  pthread_mutex_lock (&b->buffer_mutex);
+  while (b->count >= N) {
+      pthread_cond_wait (&b->buffer_cond, &b->buffer_mutex);
+  }
+  b->command[b->write] = command;
+  b->count++;
+  b->write = (b->write + 1) % N;
+  pthread_cond_broadcast (&b->buffer_cond);
+	pthread_mutex_unlock (&b->buffer_mutex);
 }
 
+/*Reads char commands from the buffer b into char* command*/
 void dequeue (CommandBuffer *b, char *command) {
-    pthread_mutex_lock (&(b->buffer_mutex));
-
-    while ((b->count) == 0) {
-        pthread_cond_wait (&(b->buffer_cond), &(b->buffer_mutex));
+    pthread_mutex_lock (&b->buffer_mutex);
+    while (b->count == 0) {
+        pthread_cond_wait (&b->buffer_cond, &b->buffer_mutex);
     }
-
-    *command = (b->command[(b->read)]);
-
+    *command = (b->command[b->read]);
     b->read = (b->read + 1) % N;
     b->count--;
-    pthread_cond_broadcast (&(b->buffer_cond));
-    pthread_mutex_unlock (&(b->buffer_mutex));
-
+    pthread_cond_broadcast(&b->buffer_cond);
+    pthread_mutex_unlock(&b->buffer_mutex);
 }
 
-void nextImage(void * rawImagePaths) {
-	struct RawImages * tempRawImages = (struct RawImages *)rawImagePaths;
-	tempRawImages->currentFile++;
-	if (tempRawImages->currentFile == tempRawImages->arraySize) {
-		tempRawImages->currentFile = 0;
+/*Sends images to the teensy board*/
+void dispImage(RenderedImage * rendImages,  int currentImage) {
+	if(!parallelState) {
+		sendImagesSame(rendImages[currentImage]);
+	} else {
+		sendImagesParallel(rendImages[currentImage], orientation);
 	}
-	char * curImage = tempRawImages->filePaths[tempRawImages->currentFile];
-	//Downsample curImage
-	//Send nodes ( ͡° ͜ʖ ͡°)
-}
-
-void changeState(void * rawImagePaths) {
-	!parallelState;
-	//Downsample the current image
-	//Send nodes ( ͡° ͜ʖ ͡°)
-}
-
-void shiftOrientation(void * renderedImages) {
-	struct RenderedImages * tempRenderedImages = (struct RenderedImages *)renderedImages;
-	//how do we get the renderedimages/nodes in here
-	//Swap the nodes below
-	FILE temp = *(tempRenderedImages->node1);
-	*(tempRenderedImages->node1) = *(tempRenderedImages->node2);
-	*(tempRenderedImages->node2) = temp;
-	//Send nodes ( ͡° ͜ʖ ͡°)
 }
